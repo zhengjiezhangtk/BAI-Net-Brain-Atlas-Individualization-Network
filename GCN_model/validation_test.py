a0acd46b60ba7f54f29d47994fe892618fbcf036
Liang Ma <trichtu@gmail.com>
2024-10-14 22:13:33 -0400

    Update validation_test.py

--- GCN_model/validation_test.py
+++ GCN_model/validation_test.py
@@ -107,25 +107,25 @@ if __name__=='__main__':
     # Settings
     flags = tf.app.flags
     FLAGS = flags.FLAGS
-    flags.DEFINE_string('dataset', '/n04dat01/atlas_group/lma/HCP_test_retest/MSM/HCP_test', 'Dataset string.')  # 'cora', 'citeseer', 'pubmed'
-    flags.DEFINE_string('modeldir', '/n04dat01/atlas_group/lma/populationGCN/BAI_Net/GCN_model/tf_BNA_0_5_L/', 'Dataset string.')
+    flags.DEFINE_string('dataset', './HCP_test', 'Dataset path')  # 'cora', 'citeseer', 'pubmed'
+    flags.DEFINE_string('modeldir', './GCN_model/tf_BNA_0_5_L/', 'model path.')
     flags.DEFINE_string('model', 'gcn_cheby', 'Model llstring.')  # 'gcn', 'gcn_cheby', 'dense'
     flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
     flags.DEFINE_float('basic_learning_rate', 0.01, 'basic learning rate.')
     flags.DEFINE_integer('epochs', 50, 'Number of epochs to train.')
     flags.DEFINE_integer('hidden1', 32, 'Number of units in hidden layer 1.')
     flags.DEFINE_float('dropout', 0., 'Dropout rate (1 - keep probability).')
-    flags.DEFINE_integer('depth', 0, 'Dropout rate (1 - keep probability).')
+    flags.DEFINE_integer('depth', 0, 'GNN depth.')
     flags.DEFINE_float('weight_decay', 5e-4, 'Weight for L2 loss on embedding matrix.')
     flags.DEFINE_integer('early_stopping', 10, 'Tolerance for early stopping (# of epochs).')
     flags.DEFINE_integer('max_degree', 5, 'Maximum Chebyshev polynomial degree.')
     flags.DEFINE_integer('train_num', 100, 'sample size for training ')
     flags.DEFINE_integer('validate_num', 20, 'sample size for training ')
-    flags.DEFINE_string('hemisphere', 'R', 'cerebral cortex part')
-    flags.DEFINE_string('mode', 'train', 'cerebral cortex part')
-    flags.DEFINE_string('MSMAll', 'True', 'cerebral cortex part')
-    flags.DEFINE_string('subdir', 'None', 'cerebral cortex part')
-    flags.DEFINE_string('sublist', 'None', 'cerebral cortex part')
+    flags.DEFINE_string('hemisphere', 'R', 'hemispphere')
+    flags.DEFINE_string('mode', 'train', 'model mode')
+    flags.DEFINE_string('MSMAll', 'True', 'if use MSMAll surface in HCP dataset')
+    flags.DEFINE_string('subdir', 'None', 'optional 1:single subject directory')
+    flags.DEFINE_string('sublist', 'None', 'optional 2:subject list')
 
     # os.environ["CUDA_VISIBLE_DEVICES"] = FLAGS.card
 
