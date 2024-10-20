d79371ff5451c197ad41879f1168320d9f3167f8
Liang Ma <trichtu@gmail.com>
2024-10-20 19:05:56 -0400

    Delete pipline directory

--- pipline/space_registration.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-subdir=$1
-FA=$2
-T1=$3
-
-flirt -ref ${T1} -in ${FA}  -omat ${subdir}/xfms/DTI_2_T1_1mm.mat
-convert_xfm  -inverse ${subdir}/xfms/DTI_2_T1_1mm.mat -omat ${subdir}/xfms/T1_1mm_2_DTI.mat
